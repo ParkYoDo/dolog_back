0b863e463a5327797dff6e4fe2846c0d4ef34406
parkyodo <scarletdemon@naver.com>
2024-01-02 17:35:05 +0900

    feat : mongoDB connect

---
+++ src/models/index.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+export const onConnectMongoDB = () => {
+  mongoose.connect(
+    `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster.5m7wvho.mongodb.net/?retryWrites=true&w=majority`,
+  );
+};
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('MongoDB : connected');
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB : disconnected');
+  onConnectMongoDB();
+});
+
+db.on('reconnected', () => {
+  console.log('MongoDB : reconnected');
+});
+
+db.on('reconnectFailed', () => {
+  console.log('MongoDB : reconnectFailed');
+});
+
+db.on('error', error => {
+  console.error(`MongoDB : ${error}`);
+});
