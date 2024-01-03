4aa37afb92c79a7d9f8afef5797a5c88fecdfb0b
parkyodo <scarletdemon@naver.com>
2024-01-03 17:48:52 +0900

    test : mongoose test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,10 @@ import postRouter from '@routes/post.route';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.json('Hello');
+});
+
 router.use('/post', postRouter);
 
 export default router;
