66fbb9a8fb154d637d1fc7100f75410dce57e449
parkyodo <scarletdemon@naver.com>
2024-01-16 17:43:15 +0900

    feat : cookie-parser

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import onConnectMongoDB from '@models/index';
 import router from '@routes/index';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 // MongoDB 연결
@@ -19,6 +20,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/', router);
 
