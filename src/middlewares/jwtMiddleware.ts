3e580454f6325880ad808d33e5706976e73ef6c8
parkyodo <scarletdemon@naver.com>
2024-01-18 16:07:12 +0900

    feat : jwt middleware

--- src/middlewares/jwtMiddleware.ts
+++ src/middlewares/jwtMiddleware.ts
@@ -3,9 +3,15 @@ import authUtil from 'utils/auth.util';
 
 const jwtMiddleware = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.headers.authorization);
-  if (req.headers.authorization) {
-    const refreshToken = req.headers.authorization.split('Bearer ')[1];
-    const isValidRefreshToken = authUtil.verifyRefreshToken(refreshToken);
+  const accessToken = req.headers.authorization?.split('Bearer ')[1];
+  if (!accessToken) return res.status(403).json({ message: 'Access Token none' });
+
+  try {
+    const decoded = authUtil.verifyAccessToken(accessToken);
+    if (!decoded) return res.status(403).json({ message: 'Access Token expiration' });
+    return next();
+  } catch (error) {
+    console.log(error);
   }
 };
 
