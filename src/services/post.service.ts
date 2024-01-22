66cb08bd489919f96de1c725dfefc16dcc15d088
ParkYoDo <scarletdemon@naver.com>
2024-01-23 02:55:34 +0900

    feat : s3 presigned url

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -4,19 +4,22 @@ import aws from 'aws-sdk';
 
 const postService = {
   getPresignedUrl: async (fileName: string) => {
-    const s3 = new aws.S3({
+    aws.config.update({
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY,
       region: process.env.AWS_REGION,
       signatureVersion: 'v4',
     });
 
+    const s3 = new aws.S3();
+
     const presignedUrl = await s3.getSignedUrlPromise('putObject', {
       Bucket: process.env.AWS_BUCKET,
-      Key: fileName,
-      Expires: 30, // seconds
-      ContentType: 'image/*',
+      Key: `images/${fileName}`,
+      Expires: 60 * 60,
     });
+
+    console.log(presignedUrl);
 
     return presignedUrl;
   },
